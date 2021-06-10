encryptogroup/LEAKER

commit 00e81d0d2882c528346a72fe923bb3fd8baeb45d

Make RelationalQuery equivalence compliant to keyword attacks

leaker/api/relational_database.py
@@ -7,7 +7,7 @@ Authors: Amos Treiber
 from abc import abstractmethod
 from collections import namedtuple
 from logging import getLogger
-from typing import Union, Tuple, Iterator, Optional, Set, List, Iterable
+from typing import Union, Tuple, Iterator, Optional, Set, List, Iterable, NamedTuple
 
 from .dataset import Dataset
 from .constants import Selectivity
@@ -15,8 +15,17 @@ from .constants import Selectivity
 log = getLogger(__name__)
 
 RelationalQuery = namedtuple("RelationalQuery", ["id", "table", "attr", "value"])
-RelationalQuery.__eq__ = lambda x, y: x.id == y.id or (x.table == y.table and x.attr == y.attr and x.value == y.value)
-RelationalKeyword = RelationalQuery  # We see a "keyword" as a relational query
+
+
+def query_equality(x: RelationalQuery, y: RelationalQuery):
+    if not isinstance(x, NamedTuple) or not isinstance(y, NamedTuple):
+        return False
+    else:
+        return x.id == y.id or (x.table == y.table and x.attr == y.attr and x.value == y.value)
+
+
+RelationalQuery.__eq__ = query_equality
+RelationalKeyword = RelationalQuery  # We see a relational "keyword" as a relational query
 
 
 class RelationalDatabase(Dataset):

commit 9873814c3b0851d828e080ca1fad17cecb0bcb01

Fix wrong check of relationalquery equality check
(wrong instance introduced in 00e81d0d2882c528346a72fe923bb3fd8baeb45d)

leaker/api/relational_database.py
@@ -7,7 +7,7 @@ Authors: Amos Treiber
 from abc import abstractmethod
 from collections import namedtuple
 from logging import getLogger
-from typing import Union, Tuple, Iterator, Optional, Set, List, Iterable, NamedTuple
+from typing import Union, Tuple, Iterator, Optional, Set, List, Iterable
 
 from .dataset import Dataset
 from .constants import Selectivity
@@ -18,10 +18,13 @@ RelationalQuery = namedtuple("RelationalQuery", ["id", "table", "attr", "value"]
 
 
 def query_equality(x: RelationalQuery, y: RelationalQuery):
-    if not isinstance(x, NamedTuple) or not isinstance(y, NamedTuple):
+    ret = True
+    if not isinstance(x, RelationalQuery) or not isinstance(y, RelationalQuery):
         return False
-    else:
-        return x.id == y.id or (x.table == y.table and x.attr == y.attr and x.value == y.value)
+    elif isinstance(x.id, int) and isinstance(y.id, int):
+        ret = ret and x.id == y. id
+    ret = ret and x.table == y.table and x.attr == y.attr and x.value == y.value
+    return ret
 
 
 RelationalQuery.__eq__ = query_equality