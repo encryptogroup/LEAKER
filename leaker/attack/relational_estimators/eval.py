5a013323e63a5945325c0c82261081bdff071bab
Patrick Ehrler <hallo@patrickehrler.de>
2022-11-09 17:54:06 +0100

    add kde estimator to script

--- leaker/attack/relational_estimators/eval.py
+++ leaker/attack/relational_estimators/eval.py
@@ -5,7 +5,7 @@ import sys
 import pandas
 
 from leaker.api import Dataset, RelationalDatabase, RelationalQuery
-from leaker.attack.relational_estimators.estimator import NaruRelationalEstimator
+from leaker.attack.relational_estimators.estimator import NaruRelationalEstimator, KDERelationalEstimator
 from leaker.extension import IdentityExtension
 from leaker.sql_interface import SQLBackend
 
@@ -47,13 +47,30 @@ mimic_db.close()
 naru_est = NaruRelationalEstimator(sample=sampled_db, full=mimic_db)
 mimic_db.open()
 errors = []
-for query in mimic_db.keywords()[:1000]:
+for query in mimic_db.keywords()[:10]:
     est = max(1, naru_est.estimate(query))  # lower bound at 1 to prevent 0 division
     actual = max(1, sum(1 for _ in mimic_db(query)))
     error = max(est, actual) / min(est, actual)  # q-error, naru paper, p. 8
     errors.append(error)
 
 print('NARU:')
+print('MEDIAN: ' + str(statistics.median(errors)))
+print('.95: ' + str(statistics.quantiles(data=errors, n=100)[94]))
+print('.99: ' + str(statistics.quantiles(data=errors, n=100)[98]))
+print('MAX: ' + str(max(errors)))
+mimic_db.close()
+
+# KDE Estimator
+kde_est = KDERelationalEstimator(sample=sampled_db, full=mimic_db)
+mimic_db.open()
+errors = []
+for query in mimic_db.keywords()[:1000]:
+    est = max(1, kde_est.estimate(query))  # lower bound at 1 to prevent 0 division
+    actual = max(1, sum(1 for _ in mimic_db(query)))
+    error = max(est, actual) / min(est, actual)  # q-error, naru paper, p. 8
+    errors.append(error)
+
+print('KDE:')
 print('MEDIAN: ' + str(statistics.median(errors)))
 print('.95: ' + str(statistics.quantiles(data=errors, n=100)[94]))
 print('.99: ' + str(statistics.quantiles(data=errors, n=100)[98]))
