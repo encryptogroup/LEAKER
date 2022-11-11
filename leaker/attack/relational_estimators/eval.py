88bd098afda83f9269c0c22ee12eab9503be4752
Patrick Ehrler <hallo@patrickehrler.de>
2022-11-11 18:18:00 +0100

    improve eval script

--- leaker/attack/relational_estimators/eval.py
+++ leaker/attack/relational_estimators/eval.py
@@ -5,7 +5,8 @@ import sys
 import pandas
 
 from leaker.api import Dataset, RelationalDatabase, RelationalQuery
-from leaker.attack.relational_estimators.estimator import NaruRelationalEstimator, KDERelationalEstimator
+from leaker.attack.relational_estimators.estimator import NaruRelationalEstimator, KDERelationalEstimator, \
+    NaiveRelationalEstimator
 from leaker.extension import IdentityExtension
 from leaker.sql_interface import SQLBackend
 
@@ -26,53 +27,37 @@ log.info(f"Loaded {mimic_db.name()} data. {len(mimic_db)} documents with {len(mi
 
 sampled_db = mimic_db.sample(0.1)
 
-# NAIVE
-errors = []
-ratio = len(mimic_db.documents()) / len(sampled_db.documents())
-mimic_db.open()
-for query in mimic_db.keywords():
-    est = max(1, ratio * sum(1 for _ in sampled_db(query)))  # lower bound at 1 to prevent 0 division
-    actual = max(1, sum(1 for _ in mimic_db(query)))
-    error = max(est, actual) / min(est, actual)  # q-error, naru paper, p. 8
-    errors.append(error)
 
-print('NAIVE:')
-print('MEDIAN: ' + str(statistics.median(errors)))
-print('.95: ' + str(statistics.quantiles(data=errors, n=100)[94]))
-print('.99: ' + str(statistics.quantiles(data=errors, n=100)[98]))
-print('MAX: ' + str(max(errors)))
-mimic_db.close()
+def evaluate_estimator_rlen(estimator):
+    mimic_db.open()
+    error_value_list = []
+    for q in mimic_db.keywords()[:1000]:
+        est_value = max(1, estimator.estimate(q))  # lower bound at 1 to prevent 0 division
+        actual_value = max(1, sum(1 for _ in mimic_db(q)))
+        current_error = max(est_value, actual_value) / min(est_value, actual_value)  # q-error, naru paper, p. 8
+        error_value_list.append(current_error)
+
+    log.info('MEDIAN: ' + str(statistics.median(error_value_list)))
+    log.info('.95: ' + str(statistics.quantiles(data=error_value_list, n=100)[94]))
+    log.info('.99: ' + str(statistics.quantiles(data=error_value_list, n=100)[98]))
+    log.info('MAX: ' + str(max(error_value_list)))
+    mimic_db.close()
+
+# NAIVE
+naive_est = NaiveRelationalEstimator(sample=sampled_db, full=mimic_db, use_full=True)
+log.info('NAIVE')
+evaluate_estimator_rlen(naive_est)
 
 # NARU Estimator
 naru_est = NaruRelationalEstimator(sample=sampled_db, full=mimic_db)
-mimic_db.open()
-errors = []
-for query in mimic_db.keywords()[:10]:
-    est = max(1, naru_est.estimate(query))  # lower bound at 1 to prevent 0 division
-    actual = max(1, sum(1 for _ in mimic_db(query)))
-    error = max(est, actual) / min(est, actual)  # q-error, naru paper, p. 8
-    errors.append(error)
-
-print('NARU:')
-print('MEDIAN: ' + str(statistics.median(errors)))
-print('.95: ' + str(statistics.quantiles(data=errors, n=100)[94]))
-print('.99: ' + str(statistics.quantiles(data=errors, n=100)[98]))
-print('MAX: ' + str(max(errors)))
-mimic_db.close()
+log.info('NARU')
+evaluate_estimator_rlen(naru_est)
 
 # KDE Estimator
 kde_est = KDERelationalEstimator(sample=sampled_db, full=mimic_db)
-mimic_db.open()
-errors = []
-for query in mimic_db.keywords()[:1000]:
-    est = max(1, kde_est.estimate(query))  # lower bound at 1 to prevent 0 division
-    actual = max(1, sum(1 for _ in mimic_db(query)))
-    error = max(est, actual) / min(est, actual)  # q-error, naru paper, p. 8
-    errors.append(error)
+log.info('KDE')
+evaluate_estimator_rlen(kde_est)
 
-print('KDE:')
-print('MEDIAN: ' + str(statistics.median(errors)))
-print('.95: ' + str(statistics.quantiles(data=errors, n=100)[94]))
-print('.99: ' + str(statistics.quantiles(data=errors, n=100)[98]))
-print('MAX: ' + str(max(errors)))
-mimic_db.close()
+
+
+
