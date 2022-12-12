3436b22c411105526e42777792c7fd6b1047509c
Patrick Ehrler <hallo@patrickehrler.de>
2022-12-12 12:29:06 +0100

    add ignore_zero_one option to eval script

--- leaker/attack/relational_estimators/eval.py
+++ leaker/attack/relational_estimators/eval.py
@@ -61,23 +61,39 @@ def ErrorMetric(est_card, card):
     return max(est_card / card, card / est_card)
 
 
-def evaluate_estimator(estimator, keywords: List[str], use_cooc=False) -> Tuple[float, float, float, float]:
+def evaluate_estimator(estimator, keywords: List[str], use_cooc=False, ignore_zero_one=False) -> \
+        Tuple[float, float, float, float]:
+    """
+
+    Other Parameters
+    ----------------
+    estimator
+    keywords
+    use_cooc : bool
+        calculate selectivity based on two keywords
+    ignore_zero_one : bool
+        ignore cases where estimation is zero and true selectivity is one
+    """
     error_value_list = []
     if not use_cooc:
         _full_identity = mimic_db.get_extension(IdentityExtension)
         for q in keywords:
             est_value = estimator.estimate(q)
             actual_value = len(_full_identity.doc_ids(q))
-            current_error = ErrorMetric(est_value, actual_value)
-            error_value_list.append(current_error)
+            if not (ignore_zero_one and est_value == 0 and actual_value == 1):
+                current_error = ErrorMetric(est_value, actual_value)
+                error_value_list.append(current_error)
+            else:
+                print("IGNORE 0.1 CASE")
     else:
         _full_coocc = mimic_db.get_extension(CoOccurrenceExtension)
         for i in range(0, len(keywords)):
             for j in range(0, i + 1):
                 est_value = estimator.estimate(keywords[i], keywords[j])
                 actual_value = _full_coocc.co_occurrence(keywords[i], keywords[j])
-                current_error = ErrorMetric(est_value, actual_value)
-                error_value_list.append(current_error)
+                if not (ignore_zero_one and est_value == 0 and actual_value == 1):
+                    current_error = ErrorMetric(est_value, actual_value)
+                    error_value_list.append(current_error)
 
     median = np.median(error_value_list)
     point95 = np.quantile(error_value_list, 0.95)
@@ -93,7 +109,7 @@ def evaluate_estimator(estimator, keywords: List[str], use_cooc=False) -> Tuple[
     return median, point95, point99, maximum
 
 
-def run_rlen_eval(nr_of_evals=1, nr_of_queries=100, use_cooc=False):
+def run_rlen_eval(nr_of_evals=1, nr_of_queries=100, use_cooc=False, ignore_zero_one=False):
     if use_cooc:
         if not mimic_db.has_extension(CoOccurrenceExtension):
             mimic_db.extend_with(CoOccurrenceExtension)
@@ -112,44 +128,45 @@ def run_rlen_eval(nr_of_evals=1, nr_of_queries=100, use_cooc=False):
             sampling_est = SamplingRelationalEstimator(sample=sampled_db, full=mimic_db)
             log.info('SAMPLING')
             results_list.append(
-               ('SAMPLING-' + str(known_data_rate),) + evaluate_estimator(sampling_est, kw_sample, use_cooc))
+                ('SAMPLING-' + str(known_data_rate),) + evaluate_estimator(sampling_est, kw_sample, use_cooc, ignore_zero_one))
 
             # NAIVE
             # naive_est = NaiveRelationalEstimator(sample=sampled_db, full=mimic_db)
             # log.info('NAIVE')
-            # results_list.append(('NAIVE-' + str(known_data_rate),) + evaluate_estimator(naive_est, kw_sample, use_cooc))
+            # results_list.append(('NAIVE-' + str(known_data_rate),) + evaluate_estimator(naive_est, kw_sample, use_cooc, ignore_zero_one))
 
             # KDE Estimator
             kde_est = KDERelationalEstimator(sample=sampled_db, full=mimic_db)
             log.info('KDE')
-            results_list.append(('KDE-' + str(known_data_rate),) + evaluate_estimator(kde_est, kw_sample, use_cooc))
+            results_list.append(('KDE-' + str(known_data_rate),) + evaluate_estimator(kde_est, kw_sample, use_cooc, ignore_zero_one))
 
             # UAE Estimator
-            #uae_est = UaeRelationalEstimator(sample=sampled_db, full=mimic_db, nr_train_queries=200)
+            # uae_est = UaeRelationalEstimator(sample=sampled_db, full=mimic_db, nr_train_queries=200)
             # 20000 training queries in UAE paper
-            #log.info('UAE')
-            #results_list.append(
-            #    ('UAE-' + str(known_data_rate) + '-200',) + evaluate_estimator(uae_est, kw_sample, use_cooc))
+            # log.info('UAE')
+            # results_list.append(
+            #    ('UAE-' + str(known_data_rate) + '-200',) + evaluate_estimator(uae_est, kw_sample, use_cooc, ignore_zero_one))
 
             # Neurocard Estimator
-            #neurocard_est = NeurocardRelationalEstimator(sample=sampled_db, full=mimic_db)
-            #log.info('Neurocard')
-            #results_list.append(
-            #    ('Neurocard-' + str(known_data_rate),) + evaluate_estimator(neurocard_est, kw_sample, use_cooc))
+            # neurocard_est = NeurocardRelationalEstimator(sample=sampled_db, full=mimic_db)
+            # log.info('Neurocard')
+            # results_list.append(
+            #    ('Neurocard-' + str(known_data_rate),) + evaluate_estimator(neurocard_est, kw_sample, use_cooc, ignore_zero_one))
 
             # NARU Estimator
             naru_est = NaruRelationalEstimator(sample=sampled_db, full=mimic_db)
             log.info('NARU')
-            results_list.append(('NARU-' + str(known_data_rate),) + evaluate_estimator(naru_est, kw_sample, use_cooc))
+            results_list.append(('NARU-' + str(known_data_rate),) + evaluate_estimator(naru_est, kw_sample, use_cooc, ignore_zero_one))
 
     df = pandas.DataFrame(data=results_list, columns=['method', 'median', '.95', '.99', 'max'])
     df = df.groupby(['method']).mean()
     df = df.sort_index()
     sns_plot = sns.heatmap(df, annot=True, cmap='RdYlGn_r', fmt=".1f")
-    plt.title('cooc=' + str(use_cooc) + ', nr_of_evals=' + str(nr_of_evals) + ', nr_of_queries=' + str(nr_of_queries))
+    plt.title('cooc=' + str(use_cooc) + ', nr_of_evals=' + str(nr_of_evals) + ', nr_of_queries=' + str(nr_of_queries) +
+              ', ignore_zero_one=' + str(ignore_zero_one))
     sns_plot.figure.savefig("estimators.png", bbox_inches='tight')
     mimic_db.close()
 
 
-run_rlen_eval(nr_of_evals=5, nr_of_queries=1000, use_cooc=False)
+run_rlen_eval(nr_of_evals=5, nr_of_queries=1000, use_cooc=False, ignore_zero_one=False)
 # print(evaluate_actual_rlen(mimic_db.keywords()))
