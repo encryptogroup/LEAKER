080cafb57acbcb9058cabd2a7868dc24055f0558
Patrick Ehrler <hallo@patrickehrler.de>
2023-01-02 17:53:56 +0100

    improve eval_dmv script to include base repetitions and pseudoLowTwo selectivity

--- evaluation/keyword/eval_dmv_red.py
+++ evaluation/keyword/eval_dmv_red.py
@@ -35,29 +35,31 @@ log = logging.getLogger(__name__)
 backend = SQLBackend()
 log.info(f"has dbs {backend.data_sets()}")
 mimic_db: Dataset = backend.load("dmv_10k")
-mimic_db.extend_with(IdentityExtension)
-mimic_db = mimic_db
-mimic_db.open()
+mimic_db.extend_with(SelectivityExtension)
 #mimic_db.extend_with(CoOccurrenceExtension)
 
-log.info(f"Loaded {mimic_db.name()} data. {len(mimic_db)} documents with {len(mimic_db.keywords())} words. {mimic_db.has_extension(IdentityExtension)}")
+log.info(f"Loaded {mimic_db.name()} data. {len(mimic_db)} documents with {len(mimic_db.keywords())} words. {mimic_db.has_extension(SelectivityExtension)}")
 
 attacks = [RelationalSap, NaruRelationalSap]  # the attacks to evaluate
-runs = 1  # Amount of evaluations
+runs = 3  # Amount of evaluations
+max_keywords = 1000  # restrict base dataset to 1000 queries
+base_restrictions_repetitions = 3  # repeat restriction
+selectivity = Selectivity.Independent  # choose restriction independent
 
 # From this, we can construct a simple EvaluationCase:
-evaluation_case = EvaluationCase(attacks=attacks, dataset=mimic_db, runs=runs)
+evaluation_case = EvaluationCase(attacks=attacks, dataset=mimic_db, runs=runs, max_keywords=max_keywords,
+                                 base_restrictions_repetitions=base_restrictions_repetitions, selectivity=selectivity)
 
-kdr = [.1, .2, .4, .6]  # known data rates
+kdr = [.2, .4, .6]  # known data rates
 reuse = True  # If we reuse sampled datasets a number of times (=> we will have a 5x5 evaluation here)
 # From this, we can construct a DatasetSampler:
 dataset_sampler = DatasetSampler(kdr_samples=kdr, reuse=reuse)
 
 query_space = PartialQuerySpace  # The query space to populate. Here, we use partial sampling from
 # the data collection. With a query log, a QueryLogSpace is used.
-sel = Selectivity.Low  # When sampling queries, we use high selectivity keywords
+sel = Selectivity.PseudoLowTwo  # When sampling queries, we use high selectivity keywords
 qsp_size = 500  # Size of the query space
-sample_size = 300  # Amount of queries attacked at a time (sampled from the query space)
+sample_size = 150  # Amount of queries attacked at a time (sampled from the query space)
 allow_repetition = False  # If queries can repeat
 # From this, we can construct a QuerySelector:
 query_selector = QuerySelector(query_space=query_space, selectivity=sel, query_space_size=qsp_size, queries=sample_size,
@@ -72,5 +74,4 @@ eva = RelationalAttackEvaluator(evaluation_case=evaluation_case, dataset_sampler
 
 # And then run it:
 eva.run()
-mimic_db.close()
 
