7972571493ba55475256d582aa8388255ab827f3
Patrick Ehrler <hallo@patrickehrler.de>
2023-01-30 11:47:24 +0100

    use 15% of sampled queries for training in eval script

--- leaker/attack/relational_estimators/eval.py
+++ leaker/attack/relational_estimators/eval.py
@@ -174,13 +174,14 @@ def run_rlen_eval(nr_of_evals=1, nr_of_queries=100, sel=Selectivity.Independent,
             #results_list.append(('KDE-' + str(known_data_rate),) + evaluate_estimator(kde_est, kw_sample, use_cooc, ignore_zero_one))
 
             # UAE Estimator
-            nr_of_train_queries = min(len(sampled_db.keywords()), 100)
+            nr_of_train_queries = int(len(sampled_db.keywords()) * 0.15)
+            #nr_of_train_queries = min(len(sampled_db.keywords()), 100)
             train_queries = list(map(lambda x: [x], sample(sampled_db.keywords(), nr_of_train_queries)))
             uae_est = UaeRelationalEstimator(sample=sampled_db, full=mimic_db, train_queries=train_queries)
             # 20000 training queries in UAE paper
             log.info('UAE')
             results_list.append(
-               ('UAE-' + str(known_data_rate) + '-100',) + evaluate_estimator(uae_est, kw_sample, use_cooc, ignore_zero_one))
+               ('UAE-' + str(known_data_rate) + '-15%',) + evaluate_estimator(uae_est, kw_sample, use_cooc, ignore_zero_one))
 
             # Neurocard Estimator
             # neurocard_est = NeurocardRelationalEstimator(sample=sampled_db, full=mimic_db)
