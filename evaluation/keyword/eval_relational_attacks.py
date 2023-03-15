daa06920632dfeb7835290f527f53b5ce07fd3f1
Patrick Ehrler <hallo@patrickehrler.de>
2023-03-15 16:01:10 +0100

    rename eval script

--- evaluation/keyword/eval_relational_attacks.py
+++ evaluation/keyword/eval_relational_attacks.py
@@ -8,11 +8,13 @@ import logging
 import sys
 
 from leaker.api import Dataset, Selectivity
-from leaker.attack import PartialQuerySpace
-from leaker.attack.sap import RelationalSap
+from leaker.attack import PartialQuerySpace, Ihop, Score, RefinedScore
+from leaker.attack.ihop import PerfectRelationalIhop, RelationalIhop
+from leaker.attack.sap import RelationalSap, PerfectRelationalSap
+from leaker.attack.scoring import RelationalScoring, RelationalRefinedScoring
 from leaker.evaluation import EvaluationCase, QuerySelector, KnownDatasetSampler
-from leaker.evaluation.evaluator import KeywordAttackEvaluator
-from leaker.extension import SelectivityExtension
+from leaker.evaluation.evaluator import KeywordAttackEvaluator, RelationalAttackEvaluator
+from leaker.extension import SelectivityExtension, IdentityExtension
 from leaker.plotting import KeywordMatPlotLibSink
 from leaker.sql_interface import SQLBackend
 
@@ -22,7 +24,7 @@ f = logging.Formatter(fmt='{asctime} {levelname:8.8} {process} --- [{threadName:
 console = logging.StreamHandler(sys.stdout)
 console.setFormatter(f)
 
-file = logging.FileHandler('eval_dmv_red.log', 'w', 'utf-8')
+file = logging.FileHandler('eval_relational_attacks', 'w', 'utf-8')
 file.setFormatter(f)
 
 logging.basicConfig(handlers=[console, file], level=logging.DEBUG)
@@ -31,20 +33,19 @@ log = logging.getLogger(__name__)
 
 backend = SQLBackend()
 log.info(f"has dbs {backend.data_sets()}")
-dmv_db: Dataset = backend.load("dmv_1000k_11cols")
-dmv_db.extend_with(SelectivityExtension)
-# dmv_db.extend_with(CoOccurrenceExtension)
+rel_db: Dataset = backend.load("mimiciv_provider")
+rel_db.extend_with(IdentityExtension)
 
 log.info(
-    f"Loaded {dmv_db.name()} data. {len(dmv_db)} documents with {len(dmv_db.keywords())} words. {dmv_db.has_extension(SelectivityExtension)}")
+    f"Loaded {rel_db.name()} data. {len(rel_db)} documents with {len(rel_db.keywords())} words. {rel_db.has_extension(SelectivityExtension)}")
 
-attacks = [RelationalSap]  # the attacks to evaluate
-runs = 5  # Amount of evaluations
+attacks = [RelationalScoring]  # the attacks to evaluate
+runs = 3  # Amount of evaluations
 
 # From this, we can construct a simple EvaluationCase:
-evaluation_case = EvaluationCase(attacks=attacks, dataset=dmv_db, runs=runs)
+evaluation_case = EvaluationCase(attacks=attacks, dataset=rel_db, runs=runs)
 
-kdr = [0.01, 0.25, .5, .75, 1]  # known data rates
+kdr = [0.01, 0.25, .75]  # known data rates
 reuse = True  # If we reuse sampled datasets a number of times (=> we will have a 5x5 evaluation here)
 # From this, we can construct a DatasetSampler:
 dataset_sampler = KnownDatasetSampler(kdr_samples=kdr, reuse=reuse)
@@ -59,13 +60,13 @@ allow_repetition = False  # If queries can repeat
 query_selector = QuerySelector(query_space=query_space, selectivity=sel, query_space_size=qsp_size, queries=sample_size,
                                allow_repetition=allow_repetition)
 
-out_file = "sap_dmv_1000k_11cols_500_150(3xHigh).png"  # Output file (if desired), will be stored in data/figures
+out_file = "sap_mimiciv_provider_500_150(3xHigh).png"  # Output file (if desired), will be stored in data/figures
 
 # With these parameters, we can set up the Evaluator:
-eva = KeywordAttackEvaluator(evaluation_case=evaluation_case, dataset_sampler=dataset_sampler,
+eva = RelationalAttackEvaluator(evaluation_case=evaluation_case, dataset_sampler=dataset_sampler,
                              query_selector=query_selector,
                              sinks=KeywordMatPlotLibSink(out_file=out_file),
-                             parallelism=8)
+                             parallelism=2)
 
 # And then run it:
 eva.run()
